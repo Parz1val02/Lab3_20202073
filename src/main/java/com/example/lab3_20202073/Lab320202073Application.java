9172f538cb71342b327d6e414b1380751cb5ada8
r.b4rr10s <a20202073@pucp.edu.pe>
2023-04-11 20:27:40 -0500

    Primer commit a develop

---
+++ src/main/java/com/example/lab3_20202073/Lab320202073Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20202073;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320202073Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320202073Application.class, args);
+    }
+
+}
